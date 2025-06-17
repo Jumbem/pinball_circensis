ecc62e11be344dd93117167d4ca7aabfb7bce580
Ederson Torresini <etorresini@ifsc.edu.br>
2025-06-17 16:44:13 -0300

    Adicionar código Python para RPi com GPIO e MQTT

---
+++ hardware/raspberry-pi/README.md
@@ -0,0 +1,32 @@
+# Configuração do serviço
+
+## Raspberry Pi
+
+O acesso ao dispositivo em rede se dá por SSH com chave privada.
+
+### Systemd
+
+Para instalar o serviço via `systemd`:
+
+```sh
+# Baixar o código
+cd ~
+git clone https://github.com/Jumbem/pinball_circensis
+
+# Instalar o ambiente virtual e dependências
+cd ~/pinball_circensis
+python3 -m venv venv
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install -r hardware/raspberry-pi/requirements.txt
+
+# Instalar e ativar o serviço
+cd /etc/systemd/system
+ln -s /home/pi/pinball_circensis/hardware/raspberry-pi/pinball.service
+sudo systemctl daemon-reload
+sudo systemctl enable pinball
+sudo systemctl start pinball
+
+# Monitorar os logs do serviço
+sudo journalctl -f -u pinball.service
+```
---
+++ hardware/raspberry-pi/pinball.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from os import getenv
+import rpi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+
+load_dotenv()
+mqtt_host = getenv('MQTT_HOST', default='feira-de-jogos.dev.br')
+mqtt_port = int(getenv('MQTT_PORT', default='1883'))
+mqtt_timeout = int(getenv('MQTT_TIMEOUT', default='60'))
+mqtt_topic = getenv('MQTT_TOPIC', default='adc20251/pinball-et-circensis/')
+led = int(getenv('PIN_LED', default='11'))
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print('Conectado ao broker MQTT')
+    client.subscribe(mqtt_topic + '#', qos=1)
+
+
+def on_message(client, userdata, msg):
+    if ''.join([mqtt_topic, 'placar']) == msg.topic:
+        print(msg.topic, msg.payload.decode())
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led, GPIO.LOW)
+
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(mqtt_host, mqtt_port, mqtt_timeout)
+
+
+def loop():
+    try:
+        while True:
+            mqtt_client.loop()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        mqtt_client.disconnect()
+
+
+if __name__ == '__main__':
+    setup()
+    loop()
