bbc94913c09ae855d67c112cb577624ee4a1ffc7
Ederson Torresini <etorresini@ifsc.edu.br>
2025-07-09 21:28:43 -0300

    Pré-carregar assets ao iniciar o jogo

--- cliente/js/precarregamento.js
+++ cliente/js/precarregamento.js
@@ -21,12 +21,114 @@ export default class precarregamento extends Phaser.Scene {
   }
 
   preload() {
-    this.load.setPath("assets/");
-    //this.load.image("bg", "abertura-bg.png");
-    //this.load.spritesheet("jensonbutton", "jensonbutton.png", {
-    //  frameWidth: 40,
-    //  frameHeight: 29,
-    //});
+    this.load.audio(
+      "charliechaplin",
+      "assets/mp3/ost/charlie-chaplin-walk.mp3"
+    );
+    this.load.audio("botao", "assets/mp3/sfx/botao.mp3");
+    this.load.image("abertura", "assets/png/backgrounds/abertura2.png");
+    this.load.spritesheet("logo", "assets/png/other/logo.png", {
+      frameWidth: 315,
+      frameHeight: 215,
+    });
+    this.load.spritesheet("jogarbutton", "assets/png/buttons/jogar.png", {
+      frameWidth: 300,
+      frameHeight: 75,
+    });
+    this.load.spritesheet("rankingbutton", "assets/png/buttons/ranking.png", {
+      frameWidth: 300,
+      frameHeight: 75,
+    });
+    this.load.spritesheet("creditosbutton", "assets/png/buttons/creditos.png", {
+      frameWidth: 300,
+      frameHeight: 75,
+    });
+    this.load.spritesheet(
+      "highscorebutton",
+      "assets/png/buttons/highscore.png",
+      {
+        frameWidth: 300,
+        frameHeight: 75,
+      }
+    );
+    this.load.audio("hyperfun", "assets/mp3/ost/hyperfun.mp3");
+    this.load.audio("botao", "assets/mp3/sfx/botao.mp3");
+    this.load.image("creditos", "assets/png/backgrounds/abertura2.png");
+    this.load.spritesheet("voltar", "assets/png/buttons/voltar.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet(
+      "sona-bianca",
+      "assets/png/other/sona-bianca-300.png",
+      {
+        frameWidth: 300,
+        frameHeight: 300,
+      }
+    );
+    this.load.spritesheet(
+      "sona-junior",
+      "assets/png/other/sona-junior-300.png",
+      {
+        frameWidth: 300,
+        frameHeight: 300,
+      }
+    );
+    this.load.spritesheet("bianca", "assets/png/other/bianca.png", {
+      frameWidth: 325,
+      frameHeight: 99,
+    });
+    this.load.spritesheet("junior", "assets/png/other/junior.png", {
+      frameWidth: 325,
+      frameHeight: 99,
+    });
+    this.load.audio("gameover", "assets/mp3/ost/gameover.mp3");
+    this.load.image("gameover", "assets/png/backgrounds/gameover.png");
+    this.load.audio("cantarolando", "assets/mp3/ost/cantarolando.mp3");
+    this.load.audio("botao", "assets/mp3/sfx/botao.mp3");
+    this.load.image("jogar", "assets/png/backgrounds/jogar.png");
+    this.load.spritesheet("voltar", "assets/png/buttons/voltar.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet("upbutton", "assets/png/buttons/up.png", {
+      frameWidth: 92,
+      frameHeight: 104,
+    });
+    this.load.spritesheet("downbutton", "assets/png/buttons/down.png", {
+      frameWidth: 92,
+      frameHeight: 104,
+    });
+    this.load.spritesheet("confirmar", "assets/png/buttons/confirmar.png", {
+      frameWidth: 200,
+      frameHeight: 50,
+    });
+    this.load.audio("botao", "assets/mp3/sfx/botao.mp3");
+    this.load.audio("tiroEqueda", "assets/mp3/ost/tiro-e-queda.mp3");
+    this.load.image("ranking", "assets/png/backgrounds/ranking.png");
+    this.load.spritesheet("voltar", "assets/png/buttons/voltar.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.audio("soNasPretas", "assets/mp3/ost/so-nas-pretas.mp3");
+    this.load.audio("botao", "assets/mp3/sfx/botao.mp3");
+    this.load.image("newhighscore", "assets/png/backgrounds/newhighscore.png");
+    this.load.spritesheet("voltar", "assets/png/buttons/voltar.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+    this.load.spritesheet("upbutton", "assets/png/buttons/up.png", {
+      frameWidth: 92,
+      frameHeight: 104,
+    });
+    this.load.spritesheet("downbutton", "assets/png/buttons/down.png", {
+      frameWidth: 92,
+      frameHeight: 104,
+    });
+    this.load.spritesheet("confirmar", "assets/png/buttons/confirmar.png", {
+      frameWidth: 200,
+      frameHeight: 50,
+    });
   }
 
   create() {
